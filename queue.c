9cc172843a8095dcbdc29f32b8f984bfa8d40b12
Samarth Asija <90459209+sam-trg@users.noreply.github.com>
2023-09-26 14:15:27 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,58 +1,84 @@
-/* queue
-| |<-(MAX_SIZE-1)th index
-|5|<- rear, on addition rear+=1 items[rear]=item
-|9|
-|6|<- front, on deletion, front+=1
-|_|
-initially, front=rear=-1. on first insertion, front =0. on rest, rear+=1, on rest deletions, front+=1
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX_SIZE 6
 
-front ______________________ rear
-  <-  _10__2__7__89__789__4_  <-
-conditions: intitialize front and rear to -1
-on each addition, check if rear!=max_size-1 rear+=1, add to queue[rear],
-on each deletion, check if front!=-1 (underflow), else front -=1
-
-*/
-#include <stdio.h>
-#include <stdlib.h>
-#define MAX_SIZE 3
-
-struct Queue {
-    int items[MAX_SIZE];
-    int* front;
-    int* rear;
+struct queue
+{
+  int front, rear;
+  int items[MAX_SIZE];
 };
-typedef struct Queue* QUEUE;
-QUEUE queue;
 
-void display() {
-    int i;
-    for(i=*(queue->front);i<=*(queue->rear);i++) {
-        printf("%d ", queue->items[i]);
+typedef struct queue QUEUE;
+
+void enqueue(int element, QUEUE* qptr)
+{
+  if (qptr->rear==MAX_SIZE - 1) {
+      printf("queue full\n");
+      return;
+    }
+  qptr->rear+=1;
+  qptr->items[qptr->rear] = element;
+}
+
+void display(QUEUE* qptr)
+{
+  int i;
+  printf ("contents are\n");
+  for (i = qptr->front; i <= qptr->rear; i++) {
+    printf ("%d ", qptr->items[i]);
+  }
+}
+
+void dequeue(QUEUE* qptr)
+{
+  if (qptr->front > qptr->rear)
+    {
+      printf("queue empty\n");
+      return;
+    }
+  printf("element deleted is %d\n", qptr->items[qptr->front]);
+  qptr->front += 1;
+  if (qptr->front > qptr->rear)
+    {
+      qptr->front = 0;
+      qptr->rear = -1;
     }
 }
 
-void enqueue(int item) {
-    while(*(queue->front)==*(queue->rear)) {
-        if(*(queue->front)==-1) {
-            *(queue->front)=0;
-            *(queue->rear)+=1;
-            queue->items[*(queue->rear)]=item;
-            break;
-        }
-        printf("Queue overflow\n");
-        return;
-    *(queue->rear)+=1;
-    queue->items[*(queue->rear)]=item;
-    }
-}
+int main(void)
+{
+  QUEUE q;
+  q.front = 0;
+  q.rear = -1;
+  int choice;
+  int item;
+  for (;;) {
+    printf("1. insert 2. delete 3. display 4.exit\n");
+    scanf("%d", &choice);
+    
+    switch(choice) {
+	
+	case 1:
+	  
+	  printf("enter the element to be inserted\n");
+	  scanf("%d", &item);
+	  enqueue(item,&q);
+	  break;
+	  
+	case 2:
+	  
+	  dequeue(&q);
+	  break;
+	
+	case 3:
+	  
+	  display(&q);
+	  break;
+	  
+	case 4:
+	  exit(0);
+	}           /*end switch*/
+  }				/*end for */
+}				/*end main */
 
-int main() {
-    queue = (QUEUE)malloc(sizeof(QUEUE));
-    queue->front = (int*)malloc(sizeof(int));
-    queue->rear = (int*)malloc(sizeof(int));
-    enqueue(4);
-    enqueue(5);
-    enqueue(6);
-    display();
-}
+/* reset front and rear if queue gets empty after deletion*/
